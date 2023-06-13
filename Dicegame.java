4525fd412a798d9b87a4be4f4313ac5132a5b3a5
s1300086 <s1300086@u-aizu.ac.jp>
2023-06-13 16:05:28 +0900

    Dice game: initial revision

---
+++ Dicegame.java
@@ -0,0 +1,25 @@
+public class Dicegame{
+
+
+    public Dicegame(  ){
+
+    }
+
+    public static void main(String args[]){
+
+        int x;
+        int y;
+        int sum;
+	System.out.println("Rolling dice...");
+	
+	x = (int)Math.ceil(Math.random()*6);
+        y = (int)Math.ceil(Math.random()*6);
+	
+        System.out.println("Die 1: " + x);
+        System.out.println("Die 2: " + y);
+	
+        sum = x + y;
+	
+        System.out.println("Total value: " + sum);
+    }
+}
